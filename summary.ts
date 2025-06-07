0df4a9f52c23281148670f186a98d61f8c5be679
David Y. Stephenson <david@davidystephenson.com>
2025-06-07 11:25:16 -0400

    Private

--- summary.ts
+++ summary.ts
@@ -1,28 +1,41 @@
-// const prius = {
-//   maker: 'Toyota',
-//   distance: 0,
-//   speed: 50
-// }
-
-// type Car = typeof prius
-
 interface Car {
   maker: string
   distance: number
   speed: number
 }
 
-type CarKey = keyof Car // 'maker' | 'distance' | 'speed'
-const x: CarKey = 'speed'
-
-const civic: Car = {
-  maker: 'Honda',
-  distance: 100,
-  speed: 20
+const car: Car = {
+  maker: 'Toyota',
+  distance: 0,
+  speed: 50
 }
 
-function describeCar (car: Car, key: CarKey) {
-  const value = car[key]
-  console.log('The', key, 'is', value)
+class Boat {
+  private password = '1234'
+  private seats: number
+  private sailCount = 0
+  speed: number
+
+  constructor (speed: number) {
+    this.seats = 2
+    this.speed = speed
+  }
+
+  private congratulate () {
+    console.log('You sailed this boat more than 10 times!')
+  }
+
+  sail () {
+    this.sailCount += 1
+    if (this.sailCount > 10) {
+      this.congratulate()
+    }
+  }
 }
-describeCar(civic, 'maker')
+
+const boat = new Boat(100)
+// boat.congratulate()
+// boat.sailCount = 100
+boat.sail()
+boat.sail()
+boat.sail()
