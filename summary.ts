0164b481dc5930c7b137aedde88f22f3588bd93a
David Y. Stephenson <david@davidystephenson.com>
2025-06-07 12:21:14 -0400

    Class generics

--- summary.ts
+++ summary.ts
@@ -1,19 +1,30 @@
-interface Car {
-  maker: string
+class Motorcycle <T> {
   distance: number
   speed: number
+  rider: string
+  data: T
+
+  constructor (speed: number, rider: string, data: T) {
+    this.speed = speed
+    this.rider = rider
+    this.data = data
+    this.distance =0
+  }
+
+  report (): T {
+    // console.log('The safety rating is:', this.data.safetyRating)
+    return this.data
+  }
 }
 
+const first = new Motorcycle(100, 'Dorothy', 'Very safe')
+console.log(first.data)
 
-
-
-
-
-type Vehicle = Omit<Car, 'maker' | 'distance'>
-
-
-function combine (a: number, b: number) {
-  return [a, b]
-}
-
-type Combined = ReturnType<typeof combine>
+const second = new Motorcycle<{ safetyRating: number }>(
+  50,
+  'Zelda',
+  { safetyRating: 5 }
+  // 'Very safe'
+)
+console.log(second.data.safetyRating)
+const result = second.report()
